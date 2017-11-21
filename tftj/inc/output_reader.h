5ce3168e6d7f0cf28695049ee51997f0483f5c45
lezebulon <vincent.grosbois@gmail.com>
2017-11-21 23:27:50 +0100

    new main

--- tftj/inc/output_reader.h
+++ tftj/inc/output_reader.h
@@ -13,28 +13,60 @@ public:
 	virtual ~OutputReader() {};
 };
 
-class BaseOutputReader : public OutputReader
+class OutputCsv : public OutputReader
 {
 	const std::string* val;
+	std::vector<std::string> keys;
+	std::ofstream myfile;
+	std::vector<std::pair<bool, std::string>> values;
+
 public:
+	OutputCsv(std::vector<std::string>& a, const std::string& fileName, bool printHeader = false)
+	{
+		keys = a;
+		values.resize(keys.size(), std::make_pair(false, ""));
+		myfile.open(fileName);
+
+		if (printHeader)
+		{
+			for (int i = 0; i < keys.size() - 1; ++i)
+			{
+				myfile << keys[i] << '\t';
+			}
+			myfile << keys[keys.size() - 1] << '\n';
+		}
+	}
+
 	virtual void newRecord(const std::string& str)
 	{
 		val = &str;
+		for (int i = 0; i < values.size(); ++i)
+		{
+			values[i] = std::make_pair(false, "");
+		}
 	}
 
-	virtual void endParsing() 
+	virtual void endRecord()
 	{
+		for (int i = 0; i < keys.size() - 1; ++i)
+		{
+			myfile << values[i].second << '\t';
+		}
+		myfile << values[keys.size() - 1].second << '\n';
+	}
+
+	virtual void endParsing()
+	{
+		myfile.close();
 	}
 
 	virtual void received(int keyIndex, int startIndex, int endIndex)
 	{
-
+		values[keyIndex] = std::make_pair(true, val->substr(startIndex, endIndex - startIndex + 1));
 	}
 
-
-	virtual ~BaseOutputReader() {};
+	virtual ~OutputCsv() {};
 };
-
 
 
 #endif
---
+++ tftj/tftj.h
@@ -0,0 +1,8 @@
+#ifndef __TFTJ_MAIN__
+#define __TFTJ_MAIN__
+
+#include "inc\parsing.h"
+
+
+
+#endif
